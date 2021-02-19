e8a916ad8ce4d5902487d8b89d27ac003f2d5818
Guilherme Nakahata <guilhermenakahata@gmail.com>
2021-02-19 01:17:54 -0300

    [GLHEMEH] Implementa metricas RandomForest

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import numpy as np
+import pandas as pd
 from sklearn.model_selection import train_test_split, StratifiedKFold
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.svm import SVC
@@ -9,12 +9,59 @@ from sklearn.model_selection import GridSearchCV
 import matplotlib.pyplot as plt
 from sklearn.model_selection import RandomizedSearchCV
 from scipy.stats import randint as sp_randint
+import sklearn.metrics as metrics
 
 def Open_DataSet():
     df = pd.read_csv("emails.csv")
     X = df.iloc[:,1:3001].values
     Y = df.iloc[:,-1].values
     return X,Y
+
+def plot_confusion_matrix(cm,
+                          target_names,
+                          title='Confusion matrix',
+                          cmap=None,
+                          normalize=True):
+
+    import matplotlib.pyplot as plt
+    import numpy as np
+    import itertools
+
+    if cmap is None:
+        cmap = plt.get_cmap('Blues')
+
+    plt.figure(figsize=(8, 6))
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+
+    cmold = cm
+
+    if target_names is not None:
+        tick_marks = np.arange(len(target_names))
+        plt.xticks(tick_marks, target_names, rotation=45)
+        plt.yticks(tick_marks, target_names)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    thresh = cm.max() / 1.5 if normalize else cm.max() / 2
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        if normalize:
+            plt.text(j, i, cmold[i, j],
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+        else:
+            plt.text(j, i, cmold[i, j],
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
+
 
 def NaiveBayes_Simple(X,Y,valueTest_size):
     train_x,test_x,train_y,test_y = train_test_split(X,Y,test_size = valueTest_size)
@@ -59,7 +106,8 @@ def SVM_K_folds(X,Y):
         print('Fold ' + str(indexFolds))
         X_train, X_val = X[train_index], X[test_index]
         y_train, y_val = Y[train_index], Y[test_index]
-        svc = SVC(C=1.0,kernel='rbf',gamma='auto')
+        # svc = SVC(C=1.0,kernel='rbf',gamma='auto')
+        svc = SVC(C=100,kernel='rbf',gamma=0.0001)
         svc.fit(X_train,y_train)
         y_pred2 = svc.predict(X_val)
         print("Score: ", accuracy_score(y_pred2,y_val))
@@ -78,6 +126,13 @@ def RandomizedSearch(X,Y):
               'min_samples_leaf': sp_randint(1,25),
               'criterion':['gini','entropy']}
 
+    tuned_parameters = [{'n_estimators': [50,60,70,80,90,100,110,120,130,140],
+                         'max_features': [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16],
+                         'max_depth': [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16],
+                         'min_samples_split': [2,3,4,5,6,10,12,13,15,20,25],
+                         'min_samples_leaf': [1,2,3,4,5,6,7,8,9,10,11,12,13,14,20],
+                         'criterion':['gini']}]
+
     rsearch = RandomizedSearchCV(rfc,
                                 param_distributions=params,
                                 n_iter=50,
@@ -86,6 +141,10 @@ def RandomizedSearch(X,Y):
                                 scoring='roc_auc',
                                 n_jobs=None,
                                 random_state=5)
+
+
+    # clf = GridSearchCV(rfc, tuned_parameters,
+    #                    scoring='roc_auc')
 
     rsearch.fit(X,Y)
 
@@ -108,7 +167,8 @@ def GridSearch(X,Y):
 
 def RandomForest(X,Y,valueTest_size):
     train_x,test_x,train_y,test_y = train_test_split(X,Y,test_size = valueTest_size)
-    rfc = RandomForestClassifier(criterion='entropy', max_depth=8,max_features=13,min_samples_leaf=6,min_samples_split=11,n_estimators=230,random_state=4)
+    rfc = RandomForestClassifier(n_estimators=100,criterion='gini')
+    # rfc = RandomForestClassifier(criterion='entropy', max_depth=8,max_features=13,min_samples_leaf=6,min_samples_split=11,n_estimators=230,random_state=4)
     rfc.fit(train_x,train_y)
 
     print("Train Results \n")
@@ -126,6 +186,18 @@ def RandomForest(X,Y,valueTest_size):
     print("Confusion Matrix for Test : \n", confusion_matrix(test_y, y_test_pred))
     print("Accuracy Score for Test : ", accuracy_score(test_y, y_test_pred))
     print("ROC AUC for Test : ", roc_auc_score(test_y, y_test_prob))
+
+    accuracy = metrics.accuracy_score(test_y, y_test_pred)
+    print('Accuracy: %f' % accuracy)
+    # precision tp / (tp + fp)
+    precision = metrics.precision_score(test_y, y_test_pred, average='macro')
+    print('Precision: %f' % precision)
+    # recall: tp / (tp + fn)
+    recall = metrics.recall_score(test_y, y_test_pred, average='macro')
+    print('Recall: %f' % recall)
+    # f1: 2 tp / (2 tp + fp + fn)
+    f1 = metrics.f1_score(test_y, y_test_pred, average='macro')
+    print('F1 score: %f' % f1)
 
     print("ROC AUC for Train : ", roc_auc_score(train_y, y_train_prob))
 
@@ -152,6 +224,8 @@ def RandomForest_K_folds(X,Y):
     kf.get_n_splits(X)
     cv_scores = list()
     indexFolds = 1
+    y_val_stringGlobal = []
+    predGlobal = []
     for train_index, test_index in kf.split(X,Y):
         print('-----------------------------------------------------------------')
         print('Fold ' + str(indexFolds))
@@ -159,19 +233,60 @@ def RandomForest_K_folds(X,Y):
         y_train, y_val = Y[train_index], Y[test_index]
         rfc = RandomForestClassifier(n_estimators=100,criterion='gini')
         rfc.fit(X_train,y_train)
+
+        print("Train Results \n")
+        y_train_pred  = rfc.predict(X_train)
+        y_train_prob = rfc.predict_proba(X_train)[:,1]
+
+        print("Confusion Matrix for Train : \n", confusion_matrix(y_train, y_train_pred))
+        print("Accuracy Score for Train : ", accuracy_score(y_train, y_train_pred))
+        print("ROC AUC for Train : ", roc_auc_score(y_train, y_train_prob))
+
+        print("+"*50)
+        print("Test Results \n")
+        y_test_pred  = rfc.predict(X_val)
+        y_test_prob = rfc.predict_proba(X_val)[:,1]
+
+        print("Confusion Matrix for Test : \n", confusion_matrix(y_val, y_test_pred))
+        print("Accuracy Score for Test : ", accuracy_score(y_val, y_test_pred))
+        print("ROC AUC for Test : ", roc_auc_score(y_val, y_test_prob))
+
+        y_val_stringGlobal.extend(y_test_pred)
+        predGlobal.extend(y_val)
+
         y_pred3 = rfc.predict(X_val)
         print("Score: ", accuracy_score(y_pred3,y_val))
         cv_scores.append(accuracy_score(y_pred3,y_val))
+
         indexFolds = indexFolds + 1
+
     print('Estimated Accuracy %.3f (%.3f)' % (np.mean(cv_scores), np.std(cv_scores)))
+
+    accuracy = metrics.accuracy_score(predGlobal, y_val_stringGlobal)
+    print('Accuracy: %f' % accuracy)
+    # precision tp / (tp + fp)
+    precision = metrics.precision_score(predGlobal, y_val_stringGlobal, average='macro')
+    print('Precision: %f' % precision)
+    # recall: tp / (tp + fn)
+    recall = metrics.recall_score(predGlobal, y_val_stringGlobal, average='macro')
+    print('Recall: %f' % recall)
+    # f1: 2 tp / (2 tp + fp + fn)
+    f1 = metrics.f1_score(predGlobal, y_val_stringGlobal, average='macro')
+    print('F1 score: %f' % f1)
+
+
+    print("Confusion Matrix for Test : \n", confusion_matrix(predGlobal,y_val_stringGlobal))
+    Style = ['Spam', 'Not Spam']
+    plot_confusion_matrix(confusion_matrix(predGlobal,y_val_stringGlobal),Style,"Classificação Random Forest","Blues")
+
 
 X,Y = Open_DataSet()
 # GridSearch(X,Y)
 # RandomForest(X,Y,0.25)
 # RandomizedSearch(X,Y)
-SVM(X,Y,0.25)
+# SVM(X,Y,0.25)
 # NaiveBayes_Simple(X,Y,0.25)
-# RandomForest_K_folds(X,Y)
+RandomForest_K_folds(X,Y)
 # SVM_K_folds(X,Y)
 # NaiveBayes_K_folds(X,Y)
 
